af6c9ce15c7a84480c746b1095839a0c402c3b72
AmabilieMabi <vitoriaamabiliebotazolimacedom@gmail.com>
2024-02-16 12:26:33 +0000

    salve

---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,14 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+     //atributos de uma classe
+    public String nome;
+    public int idade;
+
+    // metodos, verdadeiro ou fals
+    public boolean verifcarMaioridade() {
+        return this.idade >= 18;
+
+        
+    }
+}
